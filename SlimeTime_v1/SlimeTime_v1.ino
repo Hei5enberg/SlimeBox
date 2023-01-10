49f7db97e733c68f0f4c7fa7d6deff98683f8e2e
Gijs <43003177+Hei5enberg@users.noreply.github.com>
2023-01-10 16:43:58 +0100

    SlimeTime_v2

--- SlimeTime_v1/SlimeTime_v1.ino
+++ SlimeTime_v1/SlimeTime_v1.ino
@@ -1,32 +1,34 @@
 #include <WiFi.h>
 #include "time.h"
 
-#define controlPin 15
+#define controlPin 4
 
-const char* ssid     = "****";
-const char* password = "****";
-
+const char* ssid     = "*****";
+const char* password = "*****";
 const char* ntpServer = "pool.ntp.org";
+
 unsigned long epochTime; 
 
-const int lightWarmupTime = 2500;
+const int lightWarmupTime = 3000;
+const int timeLightOnSeconds = 3; 
 
-const int frameFrequencySeconds = 60; 
-//const int recordingTimeHours = 36; // Hours * 3600 to convert to seconds
-//const long int framesToBeShot = (recordingTimeHours * 3600L) / frameFrequencySeconds;
-const int timeLightOnSeconds = 4; 
+const int frameFrequencySeconds = 60 * 5; 
 
-unsigned long nextLight = 0;
+bool firstRun = true;
+unsigned long nextRun = 0;
 
-bool startupProcedure = true;
+int frameCount = 0;
 
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.print('.');
-    delay(1000);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
   }
   Serial.println(WiFi.localIP());
 }
@@ -34,16 +36,46 @@ void initWiFi() {
 void setup() {
   Serial.begin(115200);
   pinMode(controlPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   initWiFi();
   configTime(0, 0, ntpServer);
 }
 
+int ledBlinkLength = 500;
+
 void loop() {
   epochTime = getTime();
-  Serial.print("Epoch Time: ");
-  Serial.println(epochTime);
-  delay(1000);
+
+  if (firstRun) {
+    StartupSequence();
+    firstRun = false;
+    nextRun = epochTime + 10;
+  }
+  
+  if (epochTime >= nextRun) {
+    Serial.println("Turning light on...");
+    digitalWrite(controlPin, HIGH);
+    delay(lightWarmupTime);
+
+    Serial.println();
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Camera should shoot frame");
+    delay(ledBlinkLength);
+    Serial.print("Frame: ");
+    Serial.println(frameCount);
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println();
+
+    delay((timeLightOnSeconds * 1000) - ledBlinkLength);
+    digitalWrite(controlPin, LOW);
+ 
+    nextRun = epochTime + frameFrequencySeconds;
+    Serial.print("Next run: ");
+    Serial.println(nextRun);
+  }
+  
+  delay (500);
 }
 
 void StartupSequence () {
@@ -51,17 +83,10 @@ void StartupSequence () {
   Serial.println("Starting countdown:");
   for (int i = 0; i < 3; i++) {
     Serial.println(3 - i);
-    if (i == 4) {
-      digitalWrite(controlPin, HIGH);
-      delay(5000);
-      digitalWrite(controlPin, LOW);
-      delay(250);
-    } else {
-      digitalWrite(controlPin, HIGH);
-      delay(3000);
-      digitalWrite(controlPin, LOW);
-      delay(3000);
-    }
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000); 
   }
 }
 
@@ -69,7 +94,13 @@ unsigned long getTime() {
   time_t now;
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
-    //Serial.println("Failed to obtain time");
+    Serial.println("Failed to obtain time");
+    for (int i = 0; i < 25; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+    }
     return(0);
   }
   time(&now);
